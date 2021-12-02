3692be079091d7407c4249167266fe6403a9be3d
Robrecht Cannoodt <rcannood@gmail.com>
2021-12-02 16:59:29 +0100

    add run script for 170636

---
+++ src/predict_modality/methods/submission_170636/test.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+export PIPELINE_REPO="openproblems-bio/neurips2021_multimodal_viash"
+export NXF_VER=21.04.1
+export PIPELINE_VERSION=1.4.0
+method_id=submission_170636
+task_id=predict_modality
+
+# CITE GEX2ADT
+dataset_id=openproblems_bmmc_cite_phase2_rna
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
+
+target/docker/predict_modality_methods/run/${method_id}_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --output ${pred_path}.${method_id}_run.output.h5ad
+
+# CITE ADT2GEX
+dataset_id=openproblems_bmmc_cite_phase2_mod2
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
+
+target/docker/predict_modality_methods/run/${method_id}_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --input_pretrain ${pretrain_path} \
+  --output ${pred_path}.${method_id}_run.output.h5ad
+
+# MULTIOME GEX2ATAC
+dataset_id=openproblems_bmmc_multiome_phase2_rna
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
+
+target/docker/predict_modality_methods/run/${method_id}_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --input_pretrain ${pretrain_path} \
+  --output ${pred_path}.${method_id}_run.output.h5ad
+
+# MULTIOME ATAC2GEX
+dataset_id=openproblems_bmmc_multiome_phase2_mod2
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
+
+target/docker/predict_modality_methods/run/${method_id}_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --input_pretrain ${pretrain_path} \
+  --output ${pred_path}.${method_id}_run.output.h5ad
+
+# RUN EVALUATION
+bin/nextflow run "$PIPELINE_REPO" \
+  -r "$PIPELINE_VERSION" \
+  -main-script "src/$task_id/workflows/evaluate_submission/main.nf" \
+  --solutionDir "output/datasets/$task_id" \
+  --predictions "output/predictions/$task_id/**.${method_id}_run.output.h5ad" \
+  --publishDir "output/evaluation/$task_id/$method_id/" \
+  -latest \
+  -resume \
+  -c "src/resources/nextflow_moremem.config"
