e8ce318ad86d317e3adcec530002d1c93433356b
Robrecht Cannoodt <rcannood@gmail.com>
2021-12-02 15:12:13 +0100

    update

--- src/match_modality/methods/clue/test.sh
+++ src/match_modality/methods/clue/test.sh
@@ -1,20 +1,107 @@
 #!/bin/bash
 
-DATASET_PATH=output/datasets/match_modality/openproblems_bmmc_cite_phase2_rna/openproblems_bmmc_cite_phase2_rna.censor_dataset
+export PIPELINE_REPO="openproblems-bio/neurips2021_multimodal_viash"
+export NXF_VER=21.04.1
+export PIPELINE_VERSION=1.4.0
+task_id=match_modality
+
 PRETRAIN_PATH=output/pretrain/clue/openproblems_bmmc_cite_phase2_mod2.clue_train.output_pretrain/
 PRED_PATH=output/predictions/match_modality/openproblems_bmmc_cite_phase2_rna/openproblems_bmmc_cite_phase2_rna.
 
-target/docker/match_modality_methods/train/clue_train \
-  --input_train_mod1 ${DATASET_PATH}.output_train_mod1.h5ad \
-  --input_train_mod2 ${DATASET_PATH}.output_train_mod2.h5ad \
-  --input_train_sol ${DATASET_PATH}.output_train_sol.h5ad \
-  --output_pretrain $PRETRAIN_PATH
+# CITE GEX2ADT
+dataset_id=openproblems_bmmc_cite_phase2_rna
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+pretrain_path=output/pretrain/$task_id/clue/$dataset_id.clue_train.output_pretrain/
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
 
-target/docker/match_modality_methods/train/clue_run \
-  --input_train_mod1 ${DATASET_PATH}.output_train_mod1.h5ad \
-  --input_train_mod2 ${DATASET_PATH}.output_train_mod2.h5ad \
-  --input_train_sol ${DATASET_PATH}.output_train_sol.h5ad \
-  --input_test_mod1 ${DATASET_PATH}.output_test_mod1.h5ad \
-  --input_test_mod2 ${DATASET_PATH}.output_test_mod2.h5ad \
-  --input_pretrain $PRETRAIN_PATH \
-  --output ${PRED_PATH}.clue_run.output.h5ad
+target/docker/match_modality_methods/train/clue_train \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+  --output_pretrain ${pretrain_path}
+
+target/docker/match_modality_methods/run/clue_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --input_test_mod2 ${dataset_path}.output_test_mod2.h5ad \
+  --input_pretrain ${pretrain_path} \
+  --output ${pred_path}.clue_run.output.h5ad
+
+# CITE ADT2GEX
+dataset_id=openproblems_bmmc_cite_phase2_mod2
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+# can reuse same pretrain
+# pretrain_path=output/pretrain/$task_id/clue/$dataset_id.clue_train.output_pretrain/
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
+
+# target/docker/match_modality_methods/train/clue_train \
+#   --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+#   --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+#   --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+#   --output_pretrain ${pretrain_path}
+
+target/docker/match_modality_methods/run/clue_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --input_test_mod2 ${dataset_path}.output_test_mod2.h5ad \
+  --input_pretrain ${pretrain_path} \
+  --output ${pred_path}.clue_run.output.h5ad
+
+
+# MULTIOME GEX2ATAC
+dataset_id=openproblems_bmmc_multiome_phase2_rna
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+pretrain_path=output/pretrain/$task_id/clue/$dataset_id.clue_train.output_pretrain/
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
+
+target/docker/match_modality_methods/train/clue_train \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+  --output_pretrain ${pretrain_path}
+
+target/docker/match_modality_methods/run/clue_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --input_test_mod2 ${dataset_path}.output_test_mod2.h5ad \
+  --input_pretrain ${pretrain_path} \
+  --output ${pred_path}.clue_run.output.h5ad
+
+# MULTIOME ATAC2GEX
+dataset_id=openproblems_bmmc_multiome_phase2_mod2
+dataset_path=output/datasets/$task_id/$dataset_id/$dataset_id.censor_dataset
+# can reuse same pretrains
+# pretrain_path=output/pretrain/$task_id/clue/$dataset_id.clue_train.output_pretrain/
+pred_path=output/predictions/$task_id/$dataset_id/$dataset_id
+
+# target/docker/match_modality_methods/train/clue_train \
+#   --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+#   --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+#   --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+#   --output_pretrain ${pretrain_path}
+
+target/docker/match_modality_methods/run/clue_run \
+  --input_train_mod1 ${dataset_path}.output_train_mod1.h5ad \
+  --input_train_mod2 ${dataset_path}.output_train_mod2.h5ad \
+  --input_train_sol ${dataset_path}.output_train_sol.h5ad \
+  --input_test_mod1 ${dataset_path}.output_test_mod1.h5ad \
+  --input_test_mod2 ${dataset_path}.output_test_mod2.h5ad \
+  --input_pretrain ${pretrain_path} \
+  --output ${pred_path}.clue_run.output.h5ad
+
+# RUN EVALUATION
+bin/nextflow run "$PIPELINE_REPO" \
+  -r "$PIPELINE_VERSION" \
+  -main-script "src/$task_id/workflows/evaluate_submission/main.nf" \
+  --solutionDir "output/datasets/$task_id" \
+  --predictions "output/predictions/$task_id/"'**.clue_run.output.h5ad' \
+  --publishDir "output/evaluation/$task_id/clue/" \
+  -latest \
+  -resume \
+  -c "src/resources/nextflow_moremem.config"
