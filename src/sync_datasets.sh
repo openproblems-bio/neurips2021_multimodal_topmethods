83594eefcc9dd328e08e943836c696ae4515bb20
Robrecht Cannoodt <rcannood@gmail.com>
2021-12-02 09:53:29 +0100

    add dataset sync script

---
+++ src/sync_datasets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function aws_sync {
+  CMD="$1"
+  SOURCE="$2"
+  DEST="$3"
+  # use aws cli if installed
+  if command -v aws &> /dev/null; then
+    aws s3 "$CMD" --no-sign-request "$SOURCE" "$DEST"
+  # else use aws docker container instead
+  else
+    docker run \
+      --user $(id -u):$(id -g) \
+      --rm -it \
+      -v $(pwd)/output:/output \
+      -w / \
+      amazon/aws-cli \
+      s3 "$CMD" --no-sign-request "$SOURCE" "$DEST"
+  fi
+}
+
+aws_sync sync "s3://openproblems-bio/public/phase1v2-data/" "output/datasets_phase1v2/"
+aws_sync sync "s3://openproblems-bio/public/phase2-data/" "output/datasets_phase2/"
