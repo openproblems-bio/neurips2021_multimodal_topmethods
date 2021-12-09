3cd0c5d890165d6f674400ae6a9b6fbfc85f52f3
Robrecht Cannoodt <rcannood@gmail.com>
2021-12-09 19:03:30 +0100

    also set log folder

--- src/predict_modality/methods/submission_171129/train/script.sh
+++ src/predict_modality/methods/submission_171129/train/script.sh
@@ -24,53 +24,53 @@ python "$meta_resources_dir/hetero_arg_version_v5.py" 'bf_alpha_conv4_mean_fullb
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=8000 -sb -i 'normal' \
     -t 'openproblems_bmmc_multiome_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
 
 echo "Generating 'bf_alpha_conv4_mean_fullbatch_8000_phase2_inductive_gex2atac_2.pkl'"
 python "$meta_resources_dir/hetero_arg_version_v5.py" 'bf_alpha_conv4_mean_fullbatch_8000_phase2_inductive_gex2atac_2' \
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=10000 -sb -i 'normal' \
     -t 'openproblems_bmmc_multiome_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
 
 echo "Generating 'bf_alpha_conv4_mean_fullbatch_8000_phase2_inductive_gex2atac_3.pkl'"
 python "$meta_resources_dir/hetero_arg_version_v5.py" 'bf_alpha_conv4_mean_fullbatch_8000_phase2_inductive_gex2atac_3' \
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=10000 -sb -i 'normal' \
     -t 'openproblems_bmmc_multiome_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
 
 echo "Generating 'bf_alpha_conv4_mean_fullbatch_10000_phase2_inductive_gex2atac.pkl'"
 python "$meta_resources_dir/hetero_arg_version_v5.py" 'bf_alpha_conv4_mean_fullbatch_10000_phase2_inductive_gex2atac' \
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=10000 -sb -i 'normal' \
     -t 'openproblems_bmmc_multiome_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
 
 echo "Generating 'bf_alpha_conv4_mean_fullbatch_10000_phase2_inductive_gex2adt_2.pkl'"
 python "$meta_resources_dir/hetero_arg_version_v5.py" 'bf_alpha_conv4_mean_fullbatch_10000_phase2_inductive_gex2adt_2' \
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=10000 -sb -i 'normal' -bas \
     -t 'openproblems_bmmc_cite_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
 
 echo "Generating 'bf_alpha_conv4_mean_fullbatch_12000_phase2_inductive_gex2adt_sep_2.pkl'"
 python "$meta_resources_dir/hetero_arg_version_v5.py" 'bf_alpha_conv4_mean_fullbatch_12000_phase2_inductive_gex2adt_sep_2' \
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=12000 -sb -i 'normal' -bas \
     -t 'openproblems_bmmc_cite_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
 
 echo "Generating 'bf_alpha_conv4_mean_fullbatch_15000_phase2_inductive.pkl'"
 python "$meta_resources_dir/hetero_arg_version_v5.py" 'bf_alpha_conv4_mean_fullbatch_15000_phase2_inductive' \
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=12000 -sb -i 'normal' \
     -t 'openproblems_bmmc_cite_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
 
 echo "Generating 'f_alpha_conv4_mean_fullbatch_12000_phase2_inductive_batch_speration.pkl'"
 python "$meta_resources_dir/hetero_arg_version_v5.py" 'f_alpha_conv4_mean_fullbatch_12000_phase2_inductive_batch_speration' \
     -pww 'cos' -res 'res_cat' -inres -pwagg 'alpha' -pwalpha=0.5 -bs=60000 -nm 'group' -ac 'gelu' \
     -em=2 -ro=1 -conv=4 -agg 'mean' -sf -lr=1e-2 -wd=1e-5 -hid=48 -edd=0.4 -mdd=0.2 -e=12000 -sb -i 'normal' -bas \
     -t 'openproblems_bmmc_cite_phase2_rna' \
-    -m "$par_output_pretrain" -r "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
+    -m "$par_output_pretrain" -r "$par_output_pretrain" -l "$par_output_pretrain" -d "$par_data_dir" -ef "$par_output_pretrain"
