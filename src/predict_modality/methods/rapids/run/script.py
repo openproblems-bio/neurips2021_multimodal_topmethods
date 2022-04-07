3f5d90b2becb3ce0bfc4507cd16c92ea481ffbd9
Jiwei Liu <aixueer4ever@gmail.com>
2022-04-06 19:53:35 -0700

    try docker

--- src/predict_modality/methods/rapids/run/script.py
+++ src/predict_modality/methods/rapids/run/script.py
@@ -1,38 +1,46 @@
-import logging
-import anndata as ad
+import cuml
+import cudf
+import cupy as cp
 import numpy as np
+
+import anndata as ad
+import os
+import logging
 
 from scipy.sparse import csc_matrix
 
-from sklearn.decomposition import TruncatedSVD
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.kernel_ridge import KernelRidge
-
-logging.basicConfig(level=logging.INFO)
-
-## VIASH START
+path = '../../../..'
+task = 'predict_modality'
 par = {
-    'input_train_mod1': 'sample_data/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.train_mod1.h5ad',
-    'input_train_mod2': 'sample_data/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.train_mod2.h5ad',
-    'input_test_mod1': 'sample_data/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.test_mod1.h5ad',
+    'input_train_mod1': f'{path}/sample_data/{task}/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.train_mod1.h5ad',
+    'input_train_mod2': f'{path}/sample_data/{task}/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.train_mod2.h5ad',
+    'input_test_mod1': f'{path}/sample_data/{task}/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.test_mod1.h5ad',
+    'input_test_mod2': f'{path}/sample_data/{task}/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.test_mod2.h5ad',
     'output': 'output.h5ad',
 }
-meta = { 'functionality_name': 'submission_170636' }
-## VIASH END
+meta = { 'functionality_name': 'rapids_for_loop' }
 
-logging.info('Reading `h5ad` files...')
+for k,v in par.items():
+    print(k, os.path.exists(v))
+
 input_train_mod1 = ad.read_h5ad(par['input_train_mod1'])
 input_train_mod2 = ad.read_h5ad(par['input_train_mod2'])
 input_test_mod1 = ad.read_h5ad(par['input_test_mod1'])
 
+# +
 pred_dimx = input_test_mod1.shape[0]
 pred_dimy = input_train_mod2.shape[1]
 
 feature_obs = input_train_mod1.obs
 gs_obs = input_train_mod2.obs
+# -
+
+pred_dimx, pred_dimy
 
 batches = input_train_mod1.obs.batch.unique().tolist()
 batch_len = len(batches)
+
+batches
 
 obs = input_test_mod1.obs
 var = input_train_mod2.var
@@ -62,12 +70,12 @@ logging.info(f"{mod1_type}, {mod2_type}")
 n_mod1, n_mod2, scale, alpha = n_comp_dict[(mod1_type, mod2_type)]
 logging.info(f"{n_mod1}, {n_mod2}, {scale}, {alpha}")
 
-# Do PCA on the input data
+# +
 logging.info('Models using the Truncated SVD to reduce the dimension')
 
 if n_mod1 is not None and n_mod1 < input_train.shape[1]:
-    embedder_mod1 = TruncatedSVD(n_components=n_mod1)
-    mod1_pca = embedder_mod1.fit_transform(input_train.X).astype(np.float32)
+    embedder_mod1 = cuml.TruncatedSVD(n_components=n_mod1)
+    mod1_pca = embedder_mod1.fit_transform(input_train.X.toarray()).astype(np.float32)
     train_matrix = mod1_pca[input_train.obs['group'] == 'train']
     test_matrix = mod1_pca[input_train.obs['group'] == 'test']
 else:
@@ -75,8 +83,8 @@ else:
     test_matrix = input_test_mod1.to_df().values.astype(np.float32)
 
 if n_mod2 is not None and n_mod2 < input_train_mod2.shape[1]:
-    embedder_mod2 = TruncatedSVD(n_components=n_mod2)
-    train_gs = embedder_mod2.fit_transform(input_train_mod2.X).astype(np.float32)
+    embedder_mod2 = cuml.TruncatedSVD(n_components=n_mod2)
+    train_gs = embedder_mod2.fit_transform(input_train_mod2.X.toarray()).astype(np.float32)
 else:
     train_gs = input_train_mod2.to_df().values.astype(np.float32)
 
@@ -85,6 +93,7 @@ del input_train_mod1
 del input_train_mod2
 del input_test_mod1
 
+# +
 logging.info('Running normalization ...')
 train_sd = np.std(train_matrix, axis=1).reshape(-1, 1)
 train_sd[train_sd == 0] = 1
@@ -98,10 +107,14 @@ test_norm = (test_matrix - np.mean(test_matrix, axis=1).reshape(-1, 1)) / test_s
 test_norm = test_norm.astype(np.float32)
 del test_matrix
 
-logging.info('Running KRR model ...')
+
+# -
+
+# %%time
+logging.info('Running SVM model ...')
+
 y_pred = np.zeros((pred_dimx, pred_dimy), dtype=np.float32)
 np.random.seed(1000)
-
 for _ in range(5):
     np.random.shuffle(batches)
     for batch in [batches[:batch_len//2], batches[batch_len//2:]]:
@@ -110,22 +123,21 @@ for _ in range(5):
             batch = [batches[0]]
 
         logging.info(batch)
-        kernel = RBF(length_scale = scale)
-        krr = KernelRidge(alpha=alpha, kernel=kernel)
-        logging.info('Fitting KRR ... ')
-        krr.fit(train_norm[feature_obs.batch.isin(batch)], 
-                train_gs[gs_obs.batch.isin(batch)])
-        y_pred += (krr.predict(test_norm) @ embedder_mod2.components_)
-
+        ytp = []
+        for i in range(train_gs.shape[1]):
+            svm = cuml.SVR()
+            svm.fit(train_norm[feature_obs.batch.isin(batch)], 
+                train_gs[gs_obs.batch.isin(batch)][:,i])
+            ytp.append(svm.predict(test_norm))
+        ytp = np.array(ytp).T
+        y_pred += (ytp @ embedder_mod2.components_.to_output('numpy'))
 np.clip(y_pred, a_min=0, a_max=None, out=y_pred)
 if mod2_type == "ATAC":
     np.clip(y_pred, a_min=0, a_max=1, out=y_pred)
 
 y_pred /= 10
 
-# Store as sparse matrix to be efficient. Note that this might require
-# different classifiers/embedders before-hand. Not every class is able
-# to support such data structures.
+# +
 y_pred = csc_matrix(y_pred)
 
 logging.info("Generate anndata object ...")
